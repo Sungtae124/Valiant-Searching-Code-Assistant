1fe5050d5f59acf2e591d56aa637f840a96a4f70
Sungtae Kim <tsk0204@gmail.com>
2023-11-26 14:26:06 +0900

    test extension develop

--- VSCodeAssistant-AssistCodeLingo/src/extension.ts
+++ VSCodeAssistant-AssistCodeLingo/src/extension.ts
@@ -16,7 +16,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('VSCodeAssistant-AssistCodeLingo.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Assist - Code Lingo!');
+		vscode.window.showInformationMessage('May I assist you?');
 	});
 
 	context.subscriptions.push(disposable);
