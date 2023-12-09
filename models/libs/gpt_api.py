28392c9806ec767496609ecddfbabda4e3db58c0
RyuJinHyeok <101165696+RyuJinHyeok@users.noreply.github.com>
2023-12-09 18:35:41 +0900

    add organization_id option

--- models/libs/gpt_api.py
+++ models/libs/gpt_api.py
@@ -1,7 +1,8 @@
 from openai import OpenAI
 
-KEY = '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'
-client = OpenAI(api_key=KEY)
+KEY = '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'
+ORGINIZATION_ID = 'org-tJR4mOQyItBZUWA4EwP2qVu0'
+client = OpenAI(api_key=KEY, organization=ORGINIZATION_ID)
 
 def get_response(words):
 
